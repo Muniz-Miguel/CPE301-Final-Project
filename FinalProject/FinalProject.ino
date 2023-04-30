61e1f27aa7527d85ec17fa4e9b1ef9bcda8e512a
Samuel Bacino <sbacino@nevada.unr.edu>
2023-04-30 15:56:04 -0700

    fixes for dht sensor and ready to test lcd outputting

--- FinalProject/FinalProject.ino
+++ FinalProject/FinalProject.ino
@@ -3,8 +3,19 @@
 #include <Servo.h>
 #include <RTClib.h>
 
+// B register
+volatile unsigned char* port_b = (unsigned char*) 0x25; // Setting the port_b (data register) to address 0x25 (sets bit as high or low, outputs data)
+volatile unsigned char* ddr_b = (unsigned char*) 0x24;  // Setting the ddr_b (Data Direction Register) to address 0x24 (sets it as input or output)
+volatile unsigned char* pin_b = (unsigned char*) 0x23;  // Setting pin_b (Input Pin Address) to 0x23 (Reading a value from a pin)
+
+// K register
+volatile unsigned char* pin_k = (unsigned char*) 0x106; // Setting the port_k (data register) to address 0x106 (sets bit as high or low, outputs data)
+volatile unsigned char* ddr_k = (unsigned char*) 0x107; // Setting the ddr_k (Data Direction Register) to address 0x107 (sets it as input or output)
+volatile unsigned char* port_k = (unsigned char*) 0x108; // Setting pin_k (Input Pin Address) to 0x108 (Reading a value from a pin)
 
 //Water Sensor Module
+float waterThreshold = 10 ;
+
 volatile unsigned char* my_ADMUX = (unsigned char*)0x7C;
 volatile unsigned char* my_ADCSRB = (unsigned char*)0x7B;
 volatile unsigned char* my_ADCSRA = (unsigned char*)0x7A;
@@ -15,6 +26,19 @@ volatile unsigned char* myUCSR0B = (unsigned char*)0x00C1;
 volatile unsigned char* myUCSR0C = (unsigned char*)0x00C2;
 volatile unsigned int* myUBRR0 = (unsigned int*)0x00C4;
 volatile unsigned char* myUDR0 = (unsigned char*)0x00C6;
+
+// ADC
+#define RDA 0x80
+#define TBE 0x20  
+volatile unsigned char *myUCSR0A = (unsigned char *)0x00C0;
+volatile unsigned char *myUCSR0B = (unsigned char *)0x00C1;
+volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2;
+volatile unsigned int  *myUBRR0  = (unsigned int *) 0x00C4;
+volatile unsigned char *myUDR0   = (unsigned char *)0x00C6;
+volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
+volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
+volatile unsigned char* my_ADCSRA = (unsigned char*) 0x7A;
+volatile unsigned int* my_ADC_DATA = (unsigned int*) 0x78;
 
 //DHT
 #define DHTPIN 46
@@ -31,8 +55,9 @@ LiquidCrystal lcd(8,9,4,5,6,7);
 void setup(){
 
   Serial.begin(9600) ;
-
+  
   //Initialize ADC
+  U0init(9600) ;
   adc_init() ;
 
   //Initialize DHT
@@ -49,7 +74,16 @@ void setup(){
 }
 
 void loop(){
-  RTC_Module();
+
+  if(*pin_k == B00000000){ //button is toggled off
+
+    Serial.println("DISABLED STATE") ;
+    rtcModule() ;
+    Serial.println() ;
+    disabledState() ;
+  }
+  
+  rtcModule();
   double waterLevel = waterLevelReading();
   Serial.print(waterLevel) ;
   Serial.print('\n') ;
@@ -71,6 +105,48 @@ double waterLevelReading(){
   return waterLevel ;
 }
 
+double dhtRead(){
+  float humidity = dht.readHumidity() ;
+  float temperature = dht.readTemperature(true) ;
+
+  if(isnan(humidity) || isnan(temperature)){
+    Serial.println(F("Failed to Read from DHT Sensor!")) ;
+    return ;
+  }
+  dhtToLCD(humidity, temperature) ;
+  return temperature ;
+}
+
+void dhtTOLCD(float h, float t){
+  lcd.setCursor(0,0) ;
+  lcd.print("Humidity: ") ;
+  lcd.print(h) ;
+  lcd.print("%") ;
+
+  lcd.setCursor(0,1) ;
+  lcd.print("Temp: ") ;
+  lcd.print(t) ;
+  lcd.print(" F";
+}
+
+
+void rtcModule(){
+  DateTime now = rtc.now();
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(' ');
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
+  delay(1000);
+
+}
 
 void adc_init(){
   // set up the A register
@@ -119,20 +195,23 @@ unsigned int adc_read(unsigned char adc_channel_num){
   return *my_ADC_DATA;
 }
 
-void RTC_Module(){
-  DateTime now = rtc.now();
-  Serial.print(now.year(), DEC);
-  Serial.print('/');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print(' ');
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);
-  Serial.println();
-  delay(1000);
-
+void U0init(int U0baud){
+ unsigned long FCPU = 16000000;
+ unsigned int tbaud;
+ tbaud = (FCPU / 16 / U0baud - 1);
+ // Same as (FCPU / (16 * U0baud)) - 1;
+ *myUCSR0A = 0x20;
+ *myUCSR0B = 0x18;
+ *myUCSR0C = 0x06;
+ *myUBRR0  = tbaud;
+}
+unsigned char U0kbhit(){
+  return *myUCSR0A & RDA;
+}
+unsigned char U0getchar(){
+  return *myUDR0;
+}
+void U0putchar(unsigned char U0pdata){
+  while((*myUCSR0A & TBE)==0);
+  *myUDR0 = U0pdata;
 }
