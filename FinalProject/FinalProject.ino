f64215be6584acbc7989c51f34ac9f8fa52ca486
Samuel Bacino <sbacino@nevada.unr.edu>
2023-04-30 16:01:41 -0700

    Fixes to adc code and ready to test dht and lcd output

--- FinalProject/FinalProject.ino
+++ FinalProject/FinalProject.ino
@@ -16,17 +16,6 @@ volatile unsigned char* port_k = (unsigned char*) 0x108; // Setting pin_k (Input
 //Water Sensor Module
 float waterThreshold = 10 ;
 
-volatile unsigned char* my_ADMUX = (unsigned char*)0x7C;
-volatile unsigned char* my_ADCSRB = (unsigned char*)0x7B;
-volatile unsigned char* my_ADCSRA = (unsigned char*)0x7A;
-volatile unsigned int* my_ADC_DATA = (unsigned int*)0x78;
-
-volatile unsigned char* myUCSR0A = (unsigned char*)0x00C0;
-volatile unsigned char* myUCSR0B = (unsigned char*)0x00C1;
-volatile unsigned char* myUCSR0C = (unsigned char*)0x00C2;
-volatile unsigned int* myUBRR0 = (unsigned int*)0x00C4;
-volatile unsigned char* myUDR0 = (unsigned char*)0x00C6;
-
 // ADC
 #define RDA 0x80
 #define TBE 0x20  
@@ -39,6 +28,7 @@ volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
 volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
 volatile unsigned char* my_ADCSRA = (unsigned char*) 0x7A;
 volatile unsigned int* my_ADC_DATA = (unsigned int*) 0x78;
+
 
 //DHT
 #define DHTPIN 46
@@ -80,7 +70,7 @@ void loop(){
     Serial.println("DISABLED STATE") ;
     rtcModule() ;
     Serial.println() ;
-    disabledState() ;
+    //disabledState() ;
   }
   
   rtcModule();
@@ -117,7 +107,7 @@ double dhtRead(){
   return temperature ;
 }
 
-void dhtTOLCD(float h, float t){
+void dhtToLCD(float h, float t){
   lcd.setCursor(0,0) ;
   lcd.print("Humidity: ") ;
   lcd.print(h) ;
@@ -126,7 +116,7 @@ void dhtTOLCD(float h, float t){
   lcd.setCursor(0,1) ;
   lcd.print("Temp: ") ;
   lcd.print(t) ;
-  lcd.print(" F";
+  lcd.print(" F");
 }
 
 
@@ -149,21 +139,35 @@ void rtcModule(){
 }
 
 void adc_init(){
-  // set up the A register
-  *my_ADCSRA |= 0b10000000; // set bit   7 to 1 to enable the ADC
-  *my_ADCSRA &= 0b11011111; // clear bit 5 to 0 to disable the ADC trigger mode
-  *my_ADCSRA &= 0b11110111; // clear bit 3 to 0 to disable the ADC interrupt
-  *my_ADCSRA &= 0b11111100; // clear bit 2-0 to 0 to set prescaler selection to slow reading
+  // setup the A register
+  // set bit   7 to 1 to enable the ADC
+  *my_ADCSRA |= 0b10000000;
+  // clear bit 5 to 0 to disable the ADC trigger mode
+  *my_ADCSRA &= 0b11011111;
+  // clear bit 4 to 0 to disable the ADC interrupt
+  *my_ADCSRA &= 0b11110111;
+  // clear bit 3-0 to 0 to set prescaler selection to slow reading
+  *my_ADCSRA &= 0b11111000;
+
   
-  // set up the B register
-  *my_ADCSRB &= 0b11110111; // clear bit 3 to 0 to reset the channel and gain bits
-  *my_ADCSRB &= 0b11111000; // clear bit 2-0 to 0 to set free running mode
+  // setup the B register
+  // clear bit 3 to 0 to reset the channel and gain bits
+  *my_ADCSRB &= 0b11110111;
+  // clear bit 2-0 to 0 to set free running mode
+  *my_ADCSRB &= 0b11111000;
+
   
-  // set up the MUX Register
-  *my_ADMUX  &= 0b01111111; // clear bit 7 to 0 for AVCC analog reference
-  *my_ADMUX  |= 0b01000000; // set bit   6 to 1 for AVCC analog reference
-  *my_ADMUX  &= 0b11011111; // clear bit 5 to 0 for right adjust result
-  *my_ADMUX  &= 0b11100000; // clear bit 4-0 to 0 to reset the channel and gain bits
+  // setup the MUX Register
+  // clear bit 7 to 0 for AVCC analog reference
+  *my_ADMUX &= 0b01111111;
+  // set bit   6 to 1 for AVCC analog reference
+  *my_ADMUX |= 0b01000000;
+  // clear bit 5 to 0 for right adjust result
+  *my_ADMUX &= 0b11011111;
+    // clear bit 5 to 0 for right adjust result
+  *my_ADMUX &= 0b11011111;
+  // clear bit 4-0 to 0 to reset the channel and gain bits
+  *my_ADMUX &= 0b11100000;
 }
 unsigned int adc_read(unsigned char adc_channel_num){
   // reset the channel and gain bits
