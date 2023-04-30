24912899829f83da584cfdaa4c4fef217cfa94c0
Muniz-Miguel <112589409+Muniz-Miguel@users.noreply.github.com>
2023-04-30 15:11:40 -0700

    TODAY

--- FinalProject/FinalProject.ino
+++ FinalProject/FinalProject.ino
@@ -1,25 +1,138 @@
 #include <LiquidCrystal.h>
 #include "DHT.h"
 #include <Servo.h>
-#include <DS3231.h>
+#include <RTClib.h>
+
 
 //Water Sensor Module
-volatile unsigned char* port_b = (unsigned char*) 0x25 ;
-volatile unsigned char* ddr_b = (unsigned char*) 0x24 ;
-volatile unsigned char* pin_b = (unsigned char*) 0x23 ;
+volatile unsigned char* my_ADMUX = (unsigned char*)0x7C;
+volatile unsigned char* my_ADCSRB = (unsigned char*)0x7B;
+volatile unsigned char* my_ADCSRA = (unsigned char*)0x7A;
+volatile unsigned int* my_ADC_DATA = (unsigned int*)0x78;
+
+volatile unsigned char* myUCSR0A = (unsigned char*)0x00C0;
+volatile unsigned char* myUCSR0B = (unsigned char*)0x00C1;
+volatile unsigned char* myUCSR0C = (unsigned char*)0x00C2;
+volatile unsigned int* myUBRR0 = (unsigned int*)0x00C4;
+volatile unsigned char* myUDR0 = (unsigned char*)0x00C6;
+
+//DHT
+#define DHTPIN 46
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+//RTC Module
+RTC_DS1307 rtc ;
+
+
+//LCD
+LiquidCrystal lcd(8,9,4,5,6,7);
 
 void setup(){
 
   Serial.begin(9600) ;
 
+  //Initialize ADC
+  adc_init() ;
+
+  //Initialize DHT
+  dht.begin();
+
+  //RTC Module
+  rtc.begin();
+
+  //LCD
+  lcd.begin(16,2) ;
+  lcd.print("T3STING");
+
 
 }
 
 void loop(){
-  waterLevelReading() ;
+  RTC_Module();
+  double waterLevel = waterLevelReading();
+  Serial.print(waterLevel) ;
+  Serial.print('\n') ;
+  float testHumidity = dht.readHumidity();
+  float testTemp = dht.readTemperature();
+  //Serial.print(testHumidity);
+  //Serial.print('\n');
+  //Serial.print(testTemp);
+  //Serial.print('\n');
+
+  //RTC_Module();
+  lcd.setCursor(0, 1) ;
+  lcd.print(millis() / 1000);  
+  delay(1000) ;
 }
 
 double waterLevelReading(){
   unsigned int waterLevel = adc_read(0) ;
   return waterLevel ;
+}
+
+
+void adc_init(){
+  // set up the A register
+  *my_ADCSRA |= 0b10000000; // set bit   7 to 1 to enable the ADC
+  *my_ADCSRA &= 0b11011111; // clear bit 5 to 0 to disable the ADC trigger mode
+  *my_ADCSRA &= 0b11110111; // clear bit 3 to 0 to disable the ADC interrupt
+  *my_ADCSRA &= 0b11111100; // clear bit 2-0 to 0 to set prescaler selection to slow reading
+  
+  // set up the B register
+  *my_ADCSRB &= 0b11110111; // clear bit 3 to 0 to reset the channel and gain bits
+  *my_ADCSRB &= 0b11111000; // clear bit 2-0 to 0 to set free running mode
+  
+  // set up the MUX Register
+  *my_ADMUX  &= 0b01111111; // clear bit 7 to 0 for AVCC analog reference
+  *my_ADMUX  |= 0b01000000; // set bit   6 to 1 for AVCC analog reference
+  *my_ADMUX  &= 0b11011111; // clear bit 5 to 0 for right adjust result
+  *my_ADMUX  &= 0b11100000; // clear bit 4-0 to 0 to reset the channel and gain bits
+}
+unsigned int adc_read(unsigned char adc_channel_num){
+  // reset the channel and gain bits
+  *my_ADMUX  &= 0b11100000;
+  
+  // clear the channel selection bits
+  *my_ADCSRB &= 0b11110111;
+  
+  // set the channel number
+  if(adc_channel_num > 7)
+  {
+    // set the channel selection bits, but remove the most significant bit (bit 3)
+    adc_channel_num -= 8;
+    
+    // set MUX bit 
+    *my_ADCSRB |= 0b00001000;
+  }
+  
+  // set the channel selection bits
+  *my_ADMUX  += adc_channel_num;
+  
+  // set bit ?? of ADCSRA to 1 to start a conversion
+  *my_ADCSRA |= 0b01000000;
+  
+  // wait for the conversion to complete
+  while((*my_ADCSRA & 0x40) != 0);
+  
+  // return the result in the ADC data register
+  return *my_ADC_DATA;
+}
+
+void RTC_Module(){
+  DateTime now = rtc.now();
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(' ');
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
+  delay(1000);
+
 }
