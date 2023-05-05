4ea400953cf596bec7f81479ffcd1acfc622ce09
Muniz-Miguel <112589409+Muniz-Miguel@users.noreply.github.com>
2023-05-05 00:18:22 -0700

    debouncing test

--- FinalProject/FinalProject.ino
+++ FinalProject/FinalProject.ino
@@ -46,6 +46,9 @@ volatile unsigned char *mySREG = (unsigned char *)0x5F;
 volatile unsigned char *myEICRA = (unsigned char *)0x69;
 volatile unsigned char *myEIMSK = (unsigned char *)0x3D;
 
+const int SWITCH_PIN = 19;
+const int LED_PIN = 37;
+
 //MACRO to turn Fan on
 #define WRITE_HIGH_PB(pin_num) *port_b |= (0x01 << pin_num) ;
 #define WRITE_LOW_PB(pin_num) *port_b &= ~(0x01 << pin_num) ;
@@ -136,7 +139,7 @@ void setup(){
   // *myEICRA &= 0b10101111; // falling edge mode for interrupt 2/3
   // *myEIMSK |= 0b00001100; // turn on interrupt 2/3
   // *mySREG |= 0b10000000;  // turn on global interrupt
-  attachInterrupt(digitalPinToInterrupt(19), onOffSwitchISR, RISING);
+  //attachInterrupt(digitalPinToInterrupt(19), onOffSwitchISR, RISING);
   //attachInterrupt(digitalPinToInterrupt(19), onOffSwitch, FALLING) ;
   // attachInterrupt(digitalPinToInterrupt(18), resetSystem, FALLING);
   // attachInterrupt(digitalPinToInterrupt(3), turnVentClockwise, FALLING);
@@ -145,6 +148,9 @@ void setup(){
   // Serial.println(F("Entered Disabled State!")) ;
   // lcd.clear();
   // disabledState();
+  pinMode(SWITCH_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(SWITCH_PIN), onOffSwitchISR, RISING);
 
   Serial.begin(9600) ;
 }
@@ -167,21 +173,33 @@ void loop(){
   //   buttonPressed = false;
   //   WRITE_HIGH_PC(0);
   // }
-  if (buttonPressed){
-  // IF Condition for Running State
-    // if(disabled == false && error == false){ // && waterLevelReading() > waterThreshold && dht.readTemperature(true) > tempThreshold){
-      //Serial.println(F("Entered Running State!")) ;
-      lcd.clear() ;
-      runningState() ;
-      //detachInterrupt(digitalPinToInterrupt(18));
-    //}
-  } else {
-      // if(disabled == true){ //button is toggled off
-      //Serial.println(F("Entered Disabled State!")) ;
-      lcd.clear();
-      disabledState();
-      // }
+  if(millis() - lastDebounceTime > debounceDelay){
+    if(buttonPressed){
+      digitalWrite(LED_PIN, HIGH);
+    } else {
+      digitalWrite(LED_PIN, LOW);
+    }
   }
+  
+
+
+  // if (buttonPressed){
+  // // IF Condition for Running State
+  //   // if(disabled == false && error == false){ // && waterLevelReading() > waterThreshold && dht.readTemperature(true) > tempThreshold){
+  //     //Serial.println(F("Entered Running State!")) ;
+  //     // lcd.clear() ;
+  //     // runningState() ;
+  //     //detachInterrupt(digitalPinToInterrupt(18));
+  //     digitalWrite(LED_PIN, HIGH);
+  //   //}
+  // } else {
+  //     // if(disabled == true){ //button is toggled off
+  //     //Serial.println(F("Entered Disabled State!")) ;
+  //     // lcd.clear();
+  //     // disabledState();
+  //     digitalWrite(LED_PIN, LOW); // turn off the LED
+  //     // }
+  // }
   // IF Condition for Idle State
   if(disabled == false && error == false && waterLevelReading() > waterThreshold && dht.readTemperature(true) <= tempThreshold){
     Serial.println(F("Entered Idle State!")) ;
@@ -431,8 +449,15 @@ void rtcModule(){
 
 //int i;
 void onOffSwitchISR(){
-  disabled = false;
-  buttonPressed = !buttonPressed;
+  // disabled = false;
+  // buttonPressed = !buttonPressed;
+
+   if(millis() - lastDebounceTime > debounceDelay){
+    disabled = false; // enable the interrupt (assuming 'disabled' is a variable controlling the interrupt)
+    buttonPressed = !buttonPressed; // toggle the state of the button
+    lastDebounceTime = millis(); // record the last time the switch was toggled
+  }
+
   //Serial.println(i++);
 }
 
